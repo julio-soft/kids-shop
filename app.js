cea5493f232690941fe02c8bf7f31ed164e6c1ba
jcgoza <jcgoza@refcfg.cu>
2022-07-18 00:48:03 -0400

    Estableciendo role al iniciar la app

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 var app = require('./server');
+const db = require("./src/models");
+const initial = require('./src/models/init')
+
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("All models were synchronized successfully.");
+  initial();
+  console.log("All initial data were inserted successfully.")
+});;
 
 const PORT = process.env.PORT || 3000;
 
---
+++ src/models/init.js
@@ -0,0 +1,20 @@
+const db = require('./index')
+
+const Role = db.role;
+
+module.exports = function initial() {
+    Role.create({
+      id: 1,
+      name: "user"
+    });
+   
+    Role.create({
+      id: 2,
+      name: "moderator"
+    });
+   
+    Role.create({
+      id: 3,
+      name: "admin"
+    });
+  }
