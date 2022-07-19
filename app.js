ab07c4c5e73660ba0d718917610576921ef6294c
jcgoza <jcgoza@refcfg.cu>
2022-07-19 17:14:21 -0400

    Estableciendo server una vez se establezca la BD

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-var app = require('./server');
+var app = require("./server");
 const db = require("./src/models");
-const initial = require('./src/models/init')
-
+const initial = require("./src/models/init");
 
 db.sequelize.sync({ force: true }).then(() => {
   console.log("All models were synchronized successfully.");
   initial();
-  console.log("All initial data were inserted successfully.")
-});;
+  console.log("All initial data were inserted successfully.");
 
-const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
+  const server = app.listen(PORT, () => {
     const host = server.address().address;
     const port = server.address().port;
     console.log("App listening at http://%s:%s", host, port);
-  })    
+  });
+});
