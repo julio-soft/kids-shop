935694d55402b77c0816f52f4b927fa00512c0c4
jcgoza <jcgoza@refcfg.cu>
2022-07-20 01:51:29 -0400

    Test autenticacion y autorizacion

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -2,19 +2,35 @@ const request = require("supertest");
 
 const app = require("../server");
 
-describe("Test Authentication and Authorization", () => {
-  test("POST /apiv1/auth/signup", (done) => {
+const db = require("../src/models");
+const initial = require("../src/models/init");
+
+// this methodo will run before
+// Applies to all tests in this file
+beforeAll(() => {
+  return db.sequelize.sync({ force: true }).then(async () => {
+    await initial();
+  });
+});
+
+afterAll(() => {
+  // return db.sequelize.sync({ force: true });
+});
+
+describe("Test Authentication and Authorization and middleware", () => {
+  test("SignUp a new user", (done) => {
     request(app)
       .post("/apiv1/auth/signup")
       .expect("Content-Type", /json/)
       .send({
         username: "julio",
-        email: "jcgoza97@gmail.com",
+        email: "julio@gmail.com",
         password: "123",
+        roles: ["user"],
       })
       .expect(201)
       .expect((res) => {
-        res.body.data.message = "User was registered successfully!";
+        res.body.message = "User was registered successfully!";
       })
       .end((err, res) => {
         if (err) return done(err);
@@ -22,7 +38,7 @@ describe("Test Authentication and Authorization", () => {
       });
   });
 
-  test("POST /apiv1/auth/signin", (done) => {
+  test("SignIn a user", (done) => {
     request(app)
       .post("/apiv1/auth/signin")
       .expect("Content-Type", /json/)
@@ -32,8 +48,52 @@ describe("Test Authentication and Authorization", () => {
       })
       .expect(200)
       .expect((res) => {
-        res.body.data.username = "julio";
-        res.body.data.email = "jcgoza97@gmail.com";
+        if (!("username" in res.body)) throw new Error("missing next key");
+        if (!("email" in res.body)) throw new Error("missing prev key");
+        if (!("roles" in res.body)) throw new Error("missing prev key");
+        if (!("accessToken" in res.body)) throw new Error("missing prev key");
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        token = res.body.accessToken;
+        return done();
+      });
+  });
+
+  test("Url is open without authentication", (done) => {
+    request(app)
+      .get("/apiv1/test/all")
+      .expect("Content-Type", /json/)
+      .expect(200, {
+        message: "Public Content.",
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test("Authenticated client can access to User url", (done) => {
+    request(app)
+      .get("/apiv1/test/user")
+      .set("x-access-token", token)
+      .expect("Content-Type", /json/)
+      .expect(200, {
+        message: "User Content.",
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test("Authenticated client can't access to the staff url", (done) => {
+    request(app)
+      .get("/apiv1/test/mod")
+      .set("x-access-token", token)
+      .expect("Content-Type", /json/)
+      .expect(403, {
+        message: "Require Moderator Role!",
       })
       .end((err, res) => {
         if (err) return done(err);
