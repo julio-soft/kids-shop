9eec9f77f61603ae1e99e9ef76bfe021ec792917
jcgoza <jcgoza@refcfg.cu>
2022-07-20 20:46:23 -0400

    Modificacion a la config global de los modelos.

--- src/models/index.js
+++ src/models/index.js
@@ -2,22 +2,20 @@ const config = require("../config/db.config");
 const Sequelize = require("sequelize");
 
 // sequelize instance
-const sequelize = new Sequelize(
-  config.DB,
-  config.USER,
-  config.PASSWORD,
-  {
-    host: config.HOST,
-    dialect: config.dialect,
-    logging: false,
-    pool: {
-      max: config.pool.max,
-      min: config.pool.min,
-      acquire: config.pool.acquire,
-      idle: config.pool.idle
-    }
-  }
-);
+const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+  host: config.HOST,
+  dialect: config.dialect,
+  logging: false,
+  define: {
+    timestamps: false,
+  },
+  pool: {
+    max: config.pool.max,
+    min: config.pool.min,
+    acquire: config.pool.acquire,
+    idle: config.pool.idle,
+  },
+});
 
 const db = {};
 db.Sequelize = Sequelize;
@@ -30,20 +28,17 @@ db.role = require("./Auth/role.model.js")(sequelize, Sequelize);
 db.role.belongsToMany(db.user, {
   through: "user_roles",
   foreignKey: "roleId",
-  otherKey: "userId"
+  otherKey: "userId",
 });
-
 
 db.user.belongsToMany(db.role, {
   through: "user_roles",
   foreignKey: "userId",
-  otherKey: "roleId"
+  otherKey: "roleId",
 });
 
 db.shop = require("./Shop/product.model")(sequelize, Sequelize);
 
-
 db.ROLES = ["user", "admin", "moderator"];
-
 
 module.exports = db;
