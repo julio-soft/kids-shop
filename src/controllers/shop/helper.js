975dfcf4c62cd5515dbc21851371e0ff86d41ac9
jcgoza <jcgoza@refcfg.cu>
2022-07-22 15:24:38 -0400

    Eliminando codigo inecesario para mayor limpieza.

--- src/controllers/shop/helper.js
+++ src/controllers/shop/helper.js
@@ -83,43 +83,6 @@ exports.filterProduct = (query) => {
   return filters;
 };
 
-exports.filterProductQuery = (filter) => {
-  const query = squel
-    .select()
-    .from("products", "p")
-    .field("p.sku", "sku")
-    .field("p.name", "name")
-    .field("p.price", "price")
-    .field("p.stock", "stock")
-    .field("p.descriprion", "description")
-    .field("p.additional_information", "additional_information");
-
-  const filtro = squel.expr();
-
-  if (filter?.sku) filtro.and("sku = ?", filter.sku);
-  if (filter?.name) filtro.and("name LIKE %?%", filter.name);
-  if (filter?.price) filtro.and("price = ?", filter.price);
-
-  if (query?.sku) filters.sku = { [Op.eq]: query?.sku };
-  if (query?.name) filters.name = { [Op.like]: `%${query?.name}%` };
-  if (query?.price) filters.price = { [Op.eq]: query?.price };
-  if (query?.stock) filters.stock = { [Op.eq]: query?.stock };
-
-  if (query?.description)
-    filters.description = { [Op.like]: `%${query?.description}%` };
-
-  if (query?.additional_information)
-    filters.additional_information = {
-      [Op.like]: `%${query?.additional_information}%`,
-    };
-
-  if (query?.category)
-    filters["$category.name$"] = { [Op.like]: `%${query?.category}%` };
-  if (query?.tag) filters["$tag.name$"] = { [Op.like]: `%${query?.tag}%` };
-
-  return filters;
-};
-
 exports.getPageOffset = (query) => {
   const pagination = {};
 
